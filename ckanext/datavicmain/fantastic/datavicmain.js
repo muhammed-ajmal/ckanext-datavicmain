c23249ce9e0fb30c05a2fd2d844f9bb79e251f36
Nathan Perry <nathan@salsadigital.com.au>
2019-08-12 11:48:24 +1200

    [DATAVIC-108] Front-end updates for Public Release field.

--- ckanext/datavicmain/fantastic/datavicmain.js
+++ ckanext/datavicmain/fantastic/datavicmain.js
@@ -1,7 +1,21 @@
+function disable_enable_private_field() {
+    var organization_visibility = jQuery('#organization_visibility').val();
+    var workflow_status = jQuery('#workflow_status').val();
+    var disabled = !(organization_visibility === 'all' && workflow_status === 'published');
+    var private_field = jQuery('#field-private');
+    if (disabled) {
+        jQuery(private_field).val('True');
+    }
+    jQuery(private_field).prop('disabled', disabled);
+}
+
   jQuery(document).ready(function() {
     jQuery('#workflow_status').on('change', function() {
-        var workflow_status = jQuery(this).val();
-        jQuery('#field-private').prop('disabled', (workflow_status == 'published' ? false : true));
+        disable_enable_private_field();
+    });
+
+    jQuery('#organization_visibility').on('change', function() {
+        disable_enable_private_field();
     });
 
     jQuery('.calendar input').datepicker({
