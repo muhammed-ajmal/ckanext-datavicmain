9ec8ef26f87c5da4955dd642f53345f5e28ac76d
Nathan Perry <nathan@salsadigital.com.au>
2020-02-18 20:04:17 +1300

    [DATAVIC-171] WIP for auto-login on sign-up.

--- ckanext/datavicmain/plugins.py
+++ ckanext/datavicmain/plugins.py
@@ -178,6 +178,8 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
             controller='ckanext.datavicmain.controller:DataVicMainController', action='historical')
         map.connect('create_core_groups', '/create_core_groups',
             controller='ckanext.datavicmain.controller:DataVicMainController', action='create_core_groups')
+        map.connect('/user/reset/{id:.*}',
+            controller='ckanext.datavicmain.controller:DataVicUserController', action='perform_reset')
         return map
 
 
