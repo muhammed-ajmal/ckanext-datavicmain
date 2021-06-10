aed355a303530b25a9de1a35b83fa04ed3e17fb5
Mark Calvert <markcalvert84@gmail.com>
2021-06-10 12:06:04 -0600

    Restoring original 'vicmaps-harvest.py' Added new wms changes into new file 'wms-vicmaps-harvest.py'

--- admin/wms-vicmaps-harvest.py
+++ admin/wms-vicmaps-harvest.py
@@ -8,15 +8,20 @@ import requests
 import xml.etree.ElementTree as ele_tree
 import json
 import re
+
+from ckan.lib.munge import munge_title_to_name
 from unidecode import unidecode
 from cStringIO import StringIO
 from pprint import pprint
+
 
 if len(sys.argv) < 2:
     sys.stderr.write('Usage: vicmaps-harvest.py IAR_CKAN_URL IAR_CKAN_API_KEY')
     sys.exit(1)
 
-ckan = ckanapi.RemoteCKAN(sys.argv[1], sys.argv[2])
+url = sys.argv[1]
+api_key = sys.argv[2]
+ckan = ckanapi.RemoteCKAN(url, api_key)
 
 # Not in use
 #odp_ckan = ckanapi.RemoteCKAN(sys.argv[3], sys.argv[4])
@@ -63,22 +68,6 @@ def get_org(organization_title):
     return organization_cache[organization_id]
 
 
-def munge_title_to_name(name):
-    '''Munge a package title into a package name.
-    '''
-    # convert spaces and separators
-    name = re.sub('[ .:/,]', '-', name)
-    # take out not-allowed characters
-    name = re.sub('[^a-zA-Z0-9-_]', '', name).lower()
-    # remove doubles
-    name = re.sub('---', '-', name)
-    name = re.sub('--', '-', name)
-    name = re.sub('--', '-', name)
-    # remove leading or trailing hyphens
-    name = name.strip('-')[:99]
-    return name
-
-
 def convert_date(date_str):
     if date_str == 'Current' or date_str == 'Not Known' or date_str == 'current':
         return datetime.now().strftime("%Y-12-31")
@@ -122,9 +111,9 @@ root = tree.getroot()
 catalogues = root.findall('.//return')[0]
 line_pat = re.compile(r'\. |: ')
 used_slugs = []
-
-datasets_created = 0
-datasets_updated = 0
+datasets_created = []
+datasets_updated = []
+datasets_errors = []
 
 # @Todo: remove this limit
 limit = 0
@@ -144,7 +133,6 @@ for catalogue in catalogues:
         #     break
         # else:
         #     limit += 1
-
         layer = {}
         for layerAttribute in layerTag:
             attId = layerAttribute.tag.split("}")[1][0:]
@@ -154,6 +142,10 @@ for catalogue in catalogues:
             else:
                 layer[attId] = layerAttribute.text
         # print json.dumps(layer)
+        # We only want to create/update datasets that have wmsURL https://digital-engagement.atlassian.net/browse/DATAVIC-308
+        if not layer['details']['wmsUrl']:
+            print("Dataset {0} does not have wms resource so skipping".format(layer['title'].strip()))
+            continue
 
         pkg_dict = {'groups': [], 'resources': [], 'extract': ''}
         update = False
@@ -165,8 +157,8 @@ for catalogue in catalogues:
         # print '==============================================================='
         # pprint(layer)
         # print '==============================================================='
-
-        pkg_name = munge_title_to_name(layer['title'].strip())
+        layer_title = layer['title'].strip()[:93] + ' - WMS'
+        pkg_name = munge_title_to_name(layer_title)
         if pkg_name in used_slugs:
             continue
         used_slugs.append(pkg_name)
@@ -176,16 +168,17 @@ for catalogue in catalogues:
         try:
             pkg_dict = ckan.action.package_show(id=pkg_name)
             new = False
-            print munge_title_to_name(layer['title']) + " existing found"
+            print(pkg_name + " existing found")
 
             if pkg_dict.get('state', "") == 'deleted':
-                print pkg_dict['name'] + " is deleted. Undeleting..."
+                print(pkg_dict['name'] + " is deleted. Undeleting...")
                 pkg_dict['state'] = 'active'
                 update = True
         except ckanapi.NotFound:
-            print munge_title_to_name(layer['title']) + " not found, must be new"
+            pass
+            # print(pkg_name + " not found, must be new")
         except ckanapi.CKANAPIError, e:
-            print "CKAN api has failed with the following error: " + str(e) + " skipping..."
+            print("CKAN api has failed with the following error: " + str(e) + " skipping...")
             continue
 
         def get_extra(key, package_dict):
@@ -219,17 +212,17 @@ for catalogue in catalogues:
         pkg_dict['public'] = 'true'
 
         if pkg_dict.get("name", "") != pkg_name:
-            print "name changed"
+            # print "name changed"
             update = True
             pkg_dict['name'] = pkg_name
 
-        if pkg_dict.get("title", "") != layer['title'].strip():
-            print "title changed"
+        if pkg_dict.get("title", "") != layer_title:
+            # print "title changed"
             update = True
-            pkg_dict['title'] = layer['title'].strip()
+            pkg_dict['title'] = layer_title
 
         if pkg_dict.get("license_id", "") != 'cc-by':
-            print "license_id changed"
+            # print "license_id changed"
             update = True
             pkg_dict['license_id'] = 'cc-by'
 
@@ -245,25 +238,25 @@ for catalogue in catalogues:
 
             if current_extract and current_extract != extract:
                 update = True
-                print "extract changed"
+                # print "extract changed"
                 pkg_dict['extras'].remove(current_extract)
 
             pkg_dict['extras'].append({'key': 'extract', 'value': extract})
 
             if pkg_dict.get("notes", "") != layer['description']['abstractData']:
                 update = True
-                print "notes changed"
+                # print "notes changed"
                 pkg_dict['notes'] = layer['description']['abstractData']
         else:
             if layer['description'].get('purpose') and pkg_dict.get("notes", "") != layer['description'][
                 'purpose'].strip():
                 update = True
-                print "notes changed"
+                # print "notes changed"
                 pkg_dict['notes'] = layer['description']['purpose'].strip()
 
             if current_extract and current_extract != layer['description']['abstractData']:
                 update = True
-                print "extract changed"
+                # print "extract changed"
                 pkg_dict['extras'].remove(current_extract)
 
             pkg_dict['extras'].append({'key': 'extract', 'value': layer['description']['abstractData']})
@@ -285,7 +278,7 @@ for catalogue in catalogues:
 
         if full_metadata_url and full_metadata_url != layer['details']['metadataUrl']:
             update = True
-            print "full_metadata_url changed"
+            # print "full_metadata_url changed"
             pkg_dict['extras'].remove(full_metadata_url)
         pkg_dict['extras'].append({'key': 'full_metadata_url', 'value': layer['details']['metadataUrl']})
 
@@ -304,10 +297,10 @@ for catalogue in catalogues:
         if not pkg_dict.get("organization") or pkg_dict.get("organization", {}).get('title', '') != \
                 layer['description']['custodian']:
             update = True
-            print "org changed"
+            # print "org changed"
             org_name = layer['description']['custodian'].strip()
             try:
-                print org_name + " search"
+                # print org_name + " search"
                 org = get_org(org_name)
                 pkg_dict['owner_org'] = org['id']
             except ckanapi.NotFound:
@@ -346,40 +339,41 @@ for catalogue in catalogues:
             wms_dict = pkg_dict['resources'][0]
             new_res = False
 
-        if wms_dict.get('name', '') != layer['title']:
+        if wms_dict.get('name', '') != layer_title:
             update = True
-            print "wms name changed"
-            wms_dict['name'] = layer['title']
+            # print "wms name changed"
+            wms_dict['name'] = layer_title
         if wms_dict.get('url', '') != layer['details']['publicOrderUrl']:
             update = True
-            print "wms url changed"
+            # print "wms url changed"
             wms_dict['url'] = layer['details']['publicOrderUrl']
         if layer['details']['wmsUrl']:
             wms_url = layer['details']['wmsUrl'].replace('httpproxy', 'publicproxy/guest') + "?layers=" + layer['name']
             wms_preview_url = layer['details']['publicPreviewUrl'].replace('httpproxy', 'publicproxy/guest')
             if wms_dict.get('wms_api_url', '') != wms_url:
                 update = True
-                print "wms url 2 changed"
+                # print "wms url 2 changed"
                 wms_dict['format'] = 'wms'
                 wms_dict['wms_url'] = wms_preview_url
                 wms_dict['wms_api_url'] = wms_url
                 wms_dict['wms_layer'] = layer['name']
                 wms_dict['visgis_preview'] = 'active'
-        if wms_dict.get('public_order_url', '').strip() != layer['details']['publicOrderUrl'].strip():
-            update = True
-            print "public_order_url changed"
-            wms_dict['public_order_url'] = layer['details']['publicOrderUrl'].strip()
+        # Removed for https://digital-engagement.atlassian.net/browse/DATAVIC-308
+        # if wms_dict.get('public_order_url', '').strip() != layer['details']['publicOrderUrl'].strip():
+        #     update = True
+        #     print "public_order_url changed"
+        #     wms_dict['public_order_url'] = layer['details']['publicOrderUrl'].strip()
         if wms_dict.get('attribution', '') != layer['details']['attribution']:
             update = True
-            print "wms attribution changed"
+            # print "wms attribution changed"
             wms_dict['attribution'] = layer['details']['attribution']
         if wms_dict.get('period_start', '') != convert_date(layer['details']['beginDate']):
             update = True
-            print "wms period_start changed"
+            # print "wms period_start changed"
             wms_dict['period_start'] = convert_date(layer['details']['beginDate'])
         if wms_dict.get('period_end', '') != convert_date(layer['details']['endDate']):
             update = True
-            print "wms period_end changed"
+            # print "wms period_end changed"
             wms_dict['period_end'] = convert_date(layer['details']['endDate'])
         # print wms_dict
         if new_res:
@@ -388,8 +382,9 @@ for catalogue in catalogues:
             pkg_dict['resources'][0] = wms_dict
 
         # Change URLs from 'http://www.data.vic.gov.au' to 'https://sdm.iar.vic.gov.au'
-        if 'public_order_url' in pkg_dict['resources'][0] and test_vicgislite_urls(pkg_dict['resources'][0]['public_order_url']):
-            pkg_dict['resources'][0]['public_order_url'] = convert_vicgislite_urls(pkg_dict['resources'][0]['public_order_url'], pkg_dict['name'])
+        # Removed for https://digital-engagement.atlassian.net/browse/DATAVIC-308
+        # if 'public_order_url' in pkg_dict['resources'][0] and test_vicgislite_urls(pkg_dict['resources'][0]['public_order_url']):
+        #     pkg_dict['resources'][0]['public_order_url'] = convert_vicgislite_urls(pkg_dict['resources'][0]['public_order_url'], pkg_dict['name'])
         if 'url' in pkg_dict['resources'][0] and test_vicgislite_urls(pkg_dict['resources'][0]['url']):
             pkg_dict['resources'][0]['url'] = convert_vicgislite_urls(pkg_dict['resources'][0]['url'], pkg_dict['name'])
 
@@ -420,21 +415,22 @@ for catalogue in catalogues:
             try:
                 if new:
                     pkg = ckan.call_action('package_create', pkg_dict)  # create a new dataset?
-                    print pkg['id'] + " created \n"
-                    datasets_created += 1
+                    print pkg['name'] + " created \n"
+                    datasets_created.append(pkg)
                 else:
                     pkg = ckan.call_action('package_update', pkg_dict)  # create a new dataset?
-                    print pkg['id'] + " updated \n"
-                    datasets_updated += 1
+                    print pkg['name'] + " updated \n"
+                    datasets_updated.append(pkg)
                 #
                 # # @Todo remove this output
                 # pprint(pkg_dict)
             except ckanapi.errors.CKANAPIError, e:
+                datasets_errors.append(pg_dict)
                 print str(e)
                 continue
 
         else:
-            print " no changes for " + layer['title'] + " \n"
+            print " no changes for " + layer_title + " \n"
             # print pkg_dict
 
 # @Todo: Figure out what this does - it involves the ODP
@@ -457,7 +453,29 @@ for catalogue in catalogues:
 
 # Output stats on number of datasets created and updated
 print '= = = = = = = = = = = = = = = = = = = = = ='
-print str(datasets_created) + ' datasets created'
-print str(datasets_updated) + ' datasets updated'
+print str(len(datasets_created)) + ' datasets created'
+print str(len(datasets_updated)) + ' datasets updated'
+print str(len(datasets_errors)) + ' datasets errors'
 print '= = = = = = = = = = = = = = = = = = = = = ='
+
+with open('sdm_datasets_created.csv', 'w') as csv:
+    header = "title,url,full_metadata_url\n"
+    csv.write(header)
+    for dataset in datasets_created:
+        row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
+        csv.write(row)
+
+with open('sdm_datasets_updated.csv', 'w') as csv:
+    header = "title,url,full_metadata_url\n"
+    csv.write(header)
+    for dataset in datasets_updated:
+        row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
+        csv.write(row)
+
+with open('sdm_datasets_errors.csv', 'w') as csv:
+    header = "title,url,full_metadata_url\n"
+    csv.write(header)
+    for dataset in datasets_errors:
+        row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
+        csv.write(row)
 
