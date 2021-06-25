9820be32bf50205ff9ab83f3d9b145e4c9d30a18
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-06-25 18:16:35 +0200

    Fix merge issues

--- ckanext/datavicmain/helpers.py
+++ ckanext/datavicmain/helpers.py
@@ -18,6 +18,12 @@ from ckan.lib.base import render_jinja2
 from ckanext.datavicmain import schema as custom_schema
 
 
+# Conditionally import the the workflow extension helpers if workflow extension enabled in .ini
+if "workflow" in config.get('ckan.plugins', False):
+    from ckanext.workflow import helpers as workflow_helpers
+    workflow_enabled = True
+
+
 log = logging.getLogger(__name__)
 
 
@@ -132,6 +138,36 @@ def option_value_to_label(field, value):
                     return option['text']
 
 
+def group_list():
+    return toolkit.get_action('group_list')({}, {'all_fields': True})
+
+
+def workflow_status_options(current_workflow_status, owner_org):
+    options = []
+    if "workflow" in config.get('ckan.plugins', False):
+        user = toolkit.g.user
+
+        #log1.debug("\n\n\n*** workflow_status_options | current_workflow_status: %s | owner_org: %s | user: %s ***\n\n\n", current_workflow_status, owner_org, user)
+        for option in workflow_helpers.get_available_workflow_statuses(current_workflow_status, owner_org, user):
+            options.append({'value': option, 'text': option.replace('_', ' ').capitalize()})
+
+        return options
+    else:
+        return [{'value': 'draft', 'text': 'Draft'}]
+
+
+def autoselect_workflow_status_option(current_workflow_status):
+    selected_option = 'draft'
+    user = toolkit.g.user
+    if authz.is_sysadmin(user):
+        selected_option = current_workflow_status
+    return selected_option
+
+
+def workflow_status_pretty(workflow_status):
+    return workflow_status.replace('_', ' ').capitalize()
+
+
 def get_organisations_allowed_to_upload_resources():
     orgs =  toolkit.config.get('ckan.organisations_allowed_to_upload_resources', ['victorian-state-budget'])
     return orgs
@@ -146,7 +182,7 @@ def user_org_can_upload():
     user = toolkit.g.user
     id = toolkit.g.id
     context = {'user': user}
-    dataset = toolkit.get_action('package_show')( context, {'id': id })
+    dataset = toolkit.get_action('package_show')(context, {'id': id})
     allowed_organisations = get_organisations_allowed_to_upload_resources()
     user_orgs = get_user_organizations(user)
     for org in user_orgs:
