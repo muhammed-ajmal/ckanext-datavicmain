17dba06a8cf59c4e7feef0815e72b967d84e70c1
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-07-22 22:03:07 +0200

    Update the script

--- admin/delete_duplicate_datasets.py
+++ admin/delete_duplicate_datasets.py
@@ -1,16 +1,14 @@
-from ckanapi import LocalCKAN, RemoteCKAN, NotFound, CKANAPIError
-import json
-import os
+from ckanapi import RemoteCKAN, NotFound, CKANAPIError
 import csv
 
 
-#url = os.environ['LAGOON_ROUTE']
+# url = os.environ['LAGOON_ROUTE']
 url = 'https://develop.discover.data.vic.gov.au'
 apikey = 'c8a89820-a159-4c84-947d-3cb55c5a6156'
 username = 'salsa'
 local = 'http://datavic-ckan.docker.amazee.io/'
 
-# localCKAN = LocalCKAN(username=username)
+# iarCKAN = iarCKAN(username=username)
 
 purged_datasets = []
 
@@ -24,20 +22,20 @@ def get_datasets_from_odp():
     rows = 1000
     start = 0
     datasets = []
-    with RemoteCKAN(url, apikey=apikey) as ckan:
-        result = ckan.action.package_search(fq='', start=start, rows=rows)
+    with RemoteCKAN(url, apikey=apikey) as odpCKAN:
+        result = odpCKAN.action.package_search(fq='', start=start, rows=rows)
 
         while result["count"] > start:
-            start = start + rows
+            start += rows
             results = result.get('results', [])
             datasets.extend(results)
-            result = ckan.action.package_search(fq='', start=start, rows=rows)
+            result = odpCKAN.action.package_search(fq='', start=start, rows=rows)
         print("There are {0} datasets".format(len(datasets)))
     return datasets
 
 
 def safe_str(obj):
-    try: 
+    try:
         return str(obj)
     except UnicodeEncodeError:
         return obj.encode('ascii', 'ignore').decode('ascii')
@@ -48,31 +46,34 @@ def main():
     datasets = get_datasets_from_odp()
     for dataset in datasets:
         # get dataset id and name
-        #dataset_id = dataset.get('id')
+        dataset_id = dataset.get('id')
         dataset_name = dataset.get('name')
-        try:
-            with RemoteCKAN(local, apikey=apikey) as localCKAN:
-                local_dataset = localCKAN.action.package_show(id=dataset_name)
-                if local_dataset.get('private'):
-                    #purge_dataset(dataset_name)
+        with RemoteCKAN(local, apikey=apikey) as iarCKAN:
+            try:
+                # import pdb; pdb.set_trace()
+                iar_dataset = iarCKAN.action.package_show(id=dataset_id)
+                if iar_dataset.get('private'):
+                    # purge_dataset(dataset_name)
                     purged_datasets.append(dataset)
-        except NotFound:
-            # purge_dataset(dataset_name)
-            purged_datasets.append(dataset)
-            # print("Dataset not found")
-        except CKANAPIError:
-            pass
-
+            except NotFound:
+                # purge_dataset(dataset_name)
+                # import pdb; pdb.set_trace()
+                try:
+                    iar_dataset = iarCKAN.action.package_show(name_or_id=dataset_name)
+                except NotFound:
+                    purged_datasets.append(dataset)
+                except CKANAPIError as e:
+                    print("Error for dataset {0} with error {1}".format(dataset_name, e))
+            except CKANAPIError as e:
+                print("Error for dataset {0} with error {1}".format(dataset_name, e))
     with open('odp_datasets_to_purge_uat.csv', 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
-        header = "title, status"
+        header = ["No., ID,  Name, Status"]
         csvwriter.writerow(header)
-        i = 1
-        for dataset in purged_datasets:
+        for i, dataset in enumerate(purged_datasets, start=1):
             print("Write {0} dataset to csv file ".format(i))
-            data = [i, safe_str(dataset.get('title')), dataset.get('private')]
+            data = [i, dataset.get('id'), safe_str(dataset.get('name')), dataset.get('private')]
             csvwriter.writerow(data)
-            i += 1
 
 
 if __name__ == "__main__":
