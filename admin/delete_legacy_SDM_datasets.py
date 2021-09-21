ee2fa687089f93890eabe53d38eedf42ceb4a76f
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-09-21 23:12:40 +0200

    [DATAVIC-364] Update delwp download link (#67)
    * Replace publicOrderUrl with wms_preview_url
    
    * Change the wms settings for the datasets
    
    * Missing datasets
    
    * Add new sdm deletion script
    
    * Print the remaining spatial datasets
    
    * Improve the scripts
    
    * Add format to logs
    
    * Remove breakpoints
    
    * Remove unnecesary script

--- admin/delete_legacy_SDM_datasets.py
+++ admin/delete_legacy_SDM_datasets.py
@@ -1,96 +1,147 @@
+
+from __future__ import print_function
+from __future__ import absolute_import
+import argparse
+import sys
+from collections import defaultdict
+from six.moves import input
+from six import text_type
 from ckanapi import RemoteCKAN, NotFound
 import json
 import os
+import ckan.logic as logic
+
+from ckanapi.errors import CKANAPIError
 
 url = os.environ['LAGOON_ROUTE']
 apikey = 'c8a89820-a159-4c84-947d-3cb55c5a6156'
 
-try:
-    with RemoteCKAN(url, apikey=apikey) as ckan:
-        start = 0
-        rows = 1000
-        datasets = []
-        datasets_failed = []
-        while start < 3000:  # There should be less then 2000 so have this as backup to exit loop
+datasets_errors = []
+_context = None
 
-            result = ckan.action.package_search(fq='groups:spatial-data', start=start, rows=rows)
-            results = result.get('results', [])
-            # If no results exist while loop
-            if len(results) == 0:
-                print('No more results')
-                break
-            print ("There are {0} spatial-data datasets".format(len(results)))
-            exit
-            for dataset in results:
-                # print(dataset)
-                # if len(dataset.get('resources', [])) > 1:
-                    # print('Dataset {0} has {1} resource\n'.format(dataset.get('name'), len(dataset.get('resources', []))))
-                for resource in dataset.get('resources', []):
-                    # print(resource)
-                    if resource.get('public_order_url', None) or 'order?email=:emailAddress' in resource.get('url', ''):
-                        # data_dict = {"id": dataset.get('id')}
-                        datasets.append(dataset)
-                        data_dict = dataset.get('id')
-                        if dataset not in datasets:
-                            f = open('legacy-sdm-datasets/{}.json'.format(dataset.get('id')), "w")
-                            f.write(json.dumps(dataset, indent=2))
-                            f.close()
-                            datasets.append(dataset)
-                        # try:
-                        #     # result = ckan.action.package_delete(id=dataset.get('id'))
-                        #     # print('Successfully deleted {}'.format(dataset.get('id')))
-                        # except Exception as ex:
-                        #     # log and ignore
-                        #     datasets_failed.append(dataset)
-                        #     print('dataset_purge: {}'.format(ex))
-                        #     print(dataset)
-                        # print('Dataset {dataset_name} has resource {res_name} with public_order_url {public_order_url}\n'.format(\
-                        #     dataset_name=dataset.get('name'), res_name=resource.get('name'), public_order_url=resource.get('public_order_url')))
-            start = start+rows
-        # print(json.dumps(datasets))
-        # print('\n'.join(datasets))
-        print("Datasets to delete {0}".format(len(datasets)))
+def get_context():
+    from ckan import model
+    import ckan.logic as logic
+    global _context
+    if not _context:
+        user = logic.get_action(u'get_site_user')(
+            {u'model': model, u'ignore_auth': True}, {})
+        _context = {u'model': model, u'session': model.Session,
+                    u'user': user[u'name']}
+    return _context
 
-        with open('sdm_datasets_deleted.csv', 'w') as csv:
-            header = "title,url,full_metadata_url\n"
-            csv.write(header)
-            for dataset in datasets:
-                for resource in dataset.get('resources', []):
-                    try:
-                        ckan.action.datastore_delete(id=resource.get('id'), force=True)
-                        print('Successfully deleted datastore for {}'.format(resource.get('resource_id')))
-                    except NotFound as ex:
-                        pass
-                        # log and ignore
-                        # print('datastore_delete: {}'.format(ex))
+def get_sdm_datasets(result):
+    datasets = []
+    for dataset in result:
+        try:
+            data_dict = logic.get_action('package_show')(get_context(), { "id": dataset })
+            datasets.append(data_dict)
+        except CKANAPIError as e:
+            print(e)
+            datasets_errors.append(dataset)
+            continue
 
-                    try:
-                        ckan.action.resource_view_delete(id=resource.get('id'), force=True)
-                        print('Successful deleted resource_view for {}'.format(resource.get('resource_id')))
-                    except NotFound as ex:
-                        pass
-                        # log and ignore
-                        # print('resource_view_delete: {}'.format(ex))
+    return datasets
+
+
+def collect_datasets():
+    start = 0
+    rows = 1000
+    datasets = []
+    datasets_failed = []
+    # while start < 3000:  # There should be less then 2000 so have this as backup to exit loop
+
+    #result = ckan.action.package_search(fq='groups:spatial-data', start=start, rows=rows)
+
+    result = logic.get_action('package_list')(get_context(), {})
+    results = get_sdm_datasets(result)
+    
+
+    print ("There are {0} spatial-data datasets".format(len(results)))
+    for dataset in results:
+
+        for resource in dataset.get('resources', []):
+            if resource.get('public_order_url', None)  or resource.get('wms_url') or 'order?email=:emailAddress' in resource.get('url', ''):
+                datasets.append(dataset)
+                data_dict = dataset.get('id')
+                if dataset not in datasets:
+                    f = open('legacy-sdm-datasets/{}.json'.format(dataset.get('id')), "w")
+                    f.write(json.dumps(dataset, indent=2))
+                    f.close()
+                    datasets.append(dataset)
+
+    print("Datasets to delete {0}".format(len(datasets)))
+
+    with open('sdm_datasets_deleted.csv', 'w') as csv:
+        header = "title,url,full_metadata_url\n"
+        csv.write(header)
+        for dataset in datasets:
+            for resource in dataset.get('resources', []):
+                try:
+                    logic.get_action('datastore_delete')(get_context(), dict(id=resource.get('id'), force=True))
+                    print('Successfully deleted datastore for {}'.format(resource.get('resource_id')))
+                except NotFound as ex:
+                    pass
+                    # log and ignore
+                    # print('datastore_delete: {}'.format(ex))
 
                 try:
-                    ckan.action.package_delete(id=dataset.get('id'))
-                    ckan.action.dataset_purge(id=dataset.get('id'))
-                    print('Successfully deleted {}'.format(dataset.get('name')))
-
-                    row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
-                    csv.write(row)
-                except Exception as ex:
+                    logic.get_action('resource_view_delete')(get_context(), dict(id=resource.get('id'), force=True))
+                    print('Successful deleted resource_view for {}'.format(resource.get('resource_id')))
+                except NotFound as ex:
+                    pass
                     # log and ignore
-                    datasets_failed.append(dataset)
-                    print('Failed to delete {0}: {1}'.format(dataset.get('name'), ex))
-                    # print(dataset)
+                    # print('resource_view_delete: {}'.format(ex))
 
-        with open('sdm_datasets_failed_to_delete.csv', 'w') as csv:
-            header = "title,url,full_metadata_url\n"
-            csv.write(header)
-            for dataset in datasets_failed:
+            try:
+                logic.get_action('package_delete')(get_context(), dict(id=dataset.get('id')))
+                logic.get_action('dataset_purge')(get_context(), dict(id=dataset.get('id')))
+                print('Successfully deleted {}'.format(dataset.get('name')))
+
                 row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
                 csv.write(row)
+            except Exception as ex:
+                # log and ignore
+                datasets_failed.append(dataset)
+                print('Failed to delete {0}: {1}'.format(dataset.get('name'), ex))
+                # print(dataset)
 
-except Exception as ex:
-    print(ex)
+    with open('sdm_datasets_failed_to_delete.csv', 'w') as csv:
+        header = "title,url,full_metadata_url\n"
+        csv.write(header)
+        for dataset in datasets_failed:
+            row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
+            csv.write(row)
+
+    with open('wms_datasets_errors.csv', 'w') as csv:
+        header = "title,url,full_metadata_url\n"
+        csv.write(header)
+        for dataset in datasets_errors:
+            row = "{0},{1}/dataset/{2},{3}\n".format(dataset.get('title').replace(',',''), url, dataset.get('name'), dataset.get('full_metadata_url'))
+            csv.write(row)
+
+if __name__ == u'__main__':
+    parser = argparse.ArgumentParser(usage=__doc__)
+    parser.add_argument(u'-c', u'--config', help=u'CKAN config file (.ini)')
+    args = parser.parse_args()
+    assert args.config, u'You must supply a --config'
+    print(u'Loading config')
+    try:
+        from ckan.cli import load_config
+        from ckan.config.middleware import make_app
+        make_app(load_config(args.config))
+    except ImportError:
+        # for CKAN 2.6 and earlier
+        def load_config(config):
+            from ckan.lib.cli import CkanCommand
+            cmd = CkanCommand(name=None)
+
+            class Options(object):
+                pass
+            cmd.options = Options()
+            cmd.options.config = config
+            cmd._load_config()
+            return
+        load_config(args.config)
+    print("Collect datasets")
+    collect_datasets()
