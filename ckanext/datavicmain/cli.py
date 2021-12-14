3e7d8688f45780a35bf80820039beac0e7b988cc
Mark Calvert <markcalvert84@gmail.com>
2021-12-14 15:46:04 +1300

    Added IClick interface to get commands Updating env variable and logging Refactored code

--- ckanext/datavicmain/cli.py
+++ ckanext/datavicmain/cli.py
@@ -1,15 +1,15 @@
-import ckan.plugins.toolkit as toolkit
-
 import click
 import logging
-import ckanext.datavicmain.jobs as jobs
+
+from ckan.plugins.toolkit import enqueue_job
+from ckanext.datavicmain import jobs
 
 log = logging.getLogger(__name__)
 
 @click.command(u"ckan-job-worker-monitor")
 def ckan_worker_job_monitor():
     try:
-        toolkit.enqueue_job(jobs.ckan_worker_job_monitor, title='CKAN job worker monitor')
+        enqueue_job(jobs.ckan_worker_job_monitor, title='CKAN job worker monitor')
         click.secho(u"CKAN job worker monitor added to worker queue", fg=u"green")
     except Exception as e:
         log.error(e)
