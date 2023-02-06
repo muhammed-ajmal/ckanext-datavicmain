48bb24b90a60a52812c8d6eca3a1463d4bbcd694
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-02-06 11:41:20 +0200

    SXDEDPCXZIC-108 / reindex organization after update

--- ckanext/datavicmain/jobs.py
+++ ckanext/datavicmain/jobs.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
 import os
 import requests
 import logging
 
+from ckan import model
+from ckan.lib.search import rebuild, commit
 
 log = logging.getLogger(__name__)
 
@@ -18,3 +22,20 @@ def ckan_worker_job_monitor():
     except requests.RequestException as e:
         log.error(f'Failed to send CKAN worker job monitor notification to {monitor_url}')
         log.error(str(e))
+
+
+def reindex_organization(id_or_name: str):
+    """Rebuild search index for all datasets inside the organization.
+    """
+    org = model.Group.get(id_or_name)
+    if not org:
+        log.warning("Organization with ID or name %s not found", id_or_name)
+        return
+
+    query = model.Session.query(model.Package.id).filter_by(owner_org=org.id)
+
+    rebuild(
+        package_ids=(p.id for p in query),
+        force=True
+    )
+    commit()
